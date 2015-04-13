0803fcbf758f0df5479bf05012a4c1447941b389
fernanda <nandasf88@gmail.com>
2015-04-13 11:47:04 -0300

    commit na branch work

--- TesteGit.java
+++ TesteGit.java
@@ -1,5 +1,5 @@
 public class TesteGit{
 	public static void main(String[] args){
-		System.out.println("Exemplo do teste de uso do git 2 ");	
+		System.out.println("Exemplo do teste de uso do git 2 da nova branch");	
 	}
 }
