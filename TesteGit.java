519ab25680348c01468522d869260df068e62c8a
fernanda <nandasf88@gmail.com>
2015-04-13 11:50:42 -0300

    commit na branch master

--- TesteGit.java
+++ TesteGit.java
@@ -1,5 +1,7 @@
 public class TesteGit{
 	public static void main(String[] args){
-		System.out.println("Exemplo do teste de uso do git 2 ");	
+		System.out.println("Exemplo do teste de uso do git 2 ");
+		System.out.println("Master");
+	
 	}
 }
